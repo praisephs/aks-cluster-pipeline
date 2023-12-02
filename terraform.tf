84eeac5812e1870a8d772b515474044750af09c5
praisephs <uyioboite@gmail.com>
2023-12-02 14:14:09 +0100

    Added aks cluster

---
+++ terraform.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.63.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~>0.9.1"
+    }
+
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "sandbox-mgmt-rg"
+    storage_account_name = "perizersandboxdevopsmgmt"
+    container_name       = "aks-container"
+    key                  = "ACCOUNT_KEY"
+
+  }
+
+}
+
